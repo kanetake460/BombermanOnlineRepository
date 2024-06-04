3649eb5ec526f5a2693ec1134973152a1859475c
kanetake <kanetake123note@outlook.jp>
2024-06-05 08:08:00 +0900

    爆弾を列挙し、引数から、それぞれを選べるようにした

--- BombermanOnline/Assets/#MyProject/Scripts/Bomb/SpesialBomb.cs
+++ BombermanOnline/Assets/#MyProject/Scripts/Bomb/SpesialBomb.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class SpesialBomb : MonoBehaviour
 {
-    CrossBomb crossBomb;
-        
-        GrenadeBomb
-        IceBomb
-        LandmineBomb
-        MegatonBomb
-        PierceBomb
+    [Header("ŠeŽíƒ{ƒ€")]
+    public GrenadeBomb grenadeBomb;
+    public LandmineBomb landmineBomb;
+    public NormalBomb RemoteBomb;
+    public CrossBomb crossBomb;
+    public NormalBomb persistentBomb;
+    public MegatonBomb megatonBomb;
+    public PierceBomb pierceBomb;
+    public NormalBomb transparentBomb;
+    public IceBomb iceBomb;
+    public FogBomb poisonBomb;
+    public FogBomb healBomb;
 
+    public enum BombType
+    {
+        GrenadeBomb,
+        LandmineBomb,
+        RemoteBomb,
+        CrossBomb,
+        PersistentBomb,
+        TransparentBomb,
+        IceBomb,
+        MegatonBomb,
+        PierceBomb,
+        HealBomb,
+        PoisonBomb,
+    };
 
+    public void Put(BombType type)
+    {
 
+    }
 
 
 
